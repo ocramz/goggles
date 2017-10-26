d4985a986cc5c860d63daebed8d383733666d525
Marco Zocca <marco.zocca@recordunion.com>
2017-10-26 16:00:22 +0200

    working on Auth

--- src/Network/Goggles/Auth.hs
+++ src/Network/Goggles/Auth.hs
@@ -1,16 +1,46 @@
 {-# language OverloadedStrings #-}
 module Network.Goggles.Auth where
 
+import Network.HTTP.Types
+import Network.HTTP.Types.URI (urlEncode, urlDecode)
+import Network.HTTP.Types.Header
 import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Data.Binary.Builder
+
 import Data.Aeson (FromJSON(..), ToJSON(..), object, (.=), encode, decode)
 
+import Control.Monad.Catch
 import Control.Concurrent.STM
 
 import qualified Data.Text as T
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as LBS
 
+import Data.Keys
 
+
+
+scopes :: [BS.ByteString]
+scopes = [ "https://www.googleapis.com/auth/cloud-platform" ]
+
+baseUrl :: BS.ByteString
+baseUrl = "https://accounts.google.com/o/oauth2/v2/auth"
+
+mkAuthHeader :: String -> Header
+mkAuthHeader tok = (hAuthorization, BS.pack $ unwords ["Bearer :",  tok])
+
+mkContentTypeHeader :: String -> Header
+mkContentTypeHeader ct = (hContentType, BS.pack ct)
+
+
+mkRequest :: MonadThrow m => String -> StdMethod -> [Header] -> m Request
+mkRequest url m heads = do
+  reqBase <- parseRequest url
+  let mbs = renderStdMethod m
+  return $ reqBase { method = mbs, requestHeaders = heads }
+
+  
 
 
 
@@ -21,10 +51,12 @@ main = do
     --         (proxyEnvironment Nothing)
     --         defaultManagerSettings
     man <- newManager defaultManagerSettings-- settings
-    let req = "http://httpbin.org/get"
-            -- Note that the following settings will be completely ignored.
-            { proxy = Nothing -- Just $ Proxy "localhost" 1234
-            }
+    req <- parseRequest "http://httpbin.org/get"
+    -- let req = "http://httpbin.org/get"
+    --         -- Note that the following settings will be completely ignored.
+    --         {
+    --           proxy = Nothing -- Just $ Proxy "localhost" 1234
+    --         }
     -- httpLbs req man >>= print
     respBS <- httpLbs req man
     print $ responseBody respBS -- (decode $ responseBody respBS :: Maybe BS.ByteString)
