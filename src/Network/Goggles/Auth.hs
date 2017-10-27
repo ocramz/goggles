9618659f4d43c28bdac0161875daf370cff6eb4e
Marco Zocca <marco.zocca@recordunion.com>
2017-10-27 15:02:04 +0200

    add get/post

--- src/Network/Goggles/Auth.hs
+++ src/Network/Goggles/Auth.hs
@@ -10,18 +10,42 @@ import Data.Binary.Builder
 
 import Data.Aeson (FromJSON(..), ToJSON(..), object, (.=), encode, decode)
 
+import Control.Monad.Reader
 import Control.Monad.Catch
 import Control.Concurrent.STM
 
+import Data.String (IsString(..))
 import qualified Data.Text as T
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as LBS
+
+import Data.Time
 
 import qualified Data.Map as M
 import Data.Typeable
 
 import Data.Keys (gcpUID, gcpPrivateKey, gcpServiceAccountSecret)
 
+
+
+data Token = Token
+    { tokenExpiresAt :: !UTCTime
+    , tokenValue     :: !T.Text
+    } deriving (Show)
+
+-- data Handle = Handle
+--     { hManager :: !Manager
+--       -- ^ Shared HTTP manager.
+--     , hToken :: !(TVar (Maybe Token))
+--       -- ^ Cache for the access token. Use 'accessToken' when within the 'Cloud'
+--       -- monad to access the token. That function will automatically refresh it
+--       -- when it is about to expire.
+--     , hFetchToken :: !(Cloud Token)
+--       -- ^ The action which is used to fetch a fresh access token.
+--     }
+
+-- newtype Cloud a = Cloud { runCloud :: ReaderT Handle }
+                
 
 
 authScopes :: [String]
@@ -37,11 +61,37 @@ mkContentTypeHeader :: String -> Header
 mkContentTypeHeader ct = (hContentType, BS.pack ct)
 
 
-mkRequest :: MonadThrow m => String -> StdMethod -> [Header] -> m Request
-mkRequest url m heads = do
-  reqBase <- parseRequest url
-  let mbs = renderStdMethod m
-  return $ reqBase { method = mbs, requestHeaders = heads }
+
+
+get :: MonadThrow m => String -> RequestHeaders -> BS.ByteString -> m Request
+get uri heads body = do
+  reqBase <- parseUrlThrow uri
+  return $ reqBase {
+      method = renderStdMethod GET
+    , requestHeaders = heads
+    , requestBody = RequestBodyBS body
+                   }
+
+post :: MonadThrow m => String -> RequestHeaders -> BS.ByteString -> m Request
+post uri heads body = do
+  reqBase <- parseUrlThrow uri
+  return $ reqBase {
+      method = renderStdMethod POST
+    , requestHeaders = heads
+    , requestBody = RequestBodyBS body
+                   }
+
+
+
+
+
+
+
+
+
+
+
+  
 
 
 withGCPCredentials :: (T.Text -> T.Text -> IO b) -> IO b
